a9c8c2898b544a3c2300e7b87d5ef067e749fd93
purple1705 <harija9080@gmail.com>
2024-11-07 20:59:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ const spinner = document.querySelector(".spinner");
 setTimeout(() => {
     spinner.style.opacity= '0';
     spinner.style.position= 'static';
+    spinner.style.loader= 'none';
 }, 1000);
